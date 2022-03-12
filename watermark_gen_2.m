a65b69cf50b78bf9271eab5477d5a8cb95b4a4f5
naiyu0609 <75236699+naiyu0609@users.noreply.github.com>
2022-03-12 20:48:44 +0800

    Delete watermark_gen_2.m

--- watermark_gen_2.m
+++
@@ -1,8 +0,0 @@
-clc, clear, close all
-im = zeros(360, 640,3);
-figure(1), imshow(im)
-text(30,60,'B10607044 潘乃聿','fontsize',20,'color','w')
-exportgraphics(figure(1), 'watermark.png'); 
-im = frame2im(getframe(gcf));
-figure,imshow(im)
-% close all
